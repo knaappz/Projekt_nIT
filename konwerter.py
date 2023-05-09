362c8fd9e75eeaa1ab74ff5d3f773165ba707c0e
Jakub <knapikj.priv@gmail.com>
2023-05-09 11:29:33 +0200

    wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku

--- konwerter.py
+++ konwerter.py
@@ -2,13 +2,19 @@ import argparse
 import json
 import yaml
 import xmltodict
+import xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(description='Konwersja plików XML, JSON i YAML.')
-
 parser.add_argument('input_file', type=str, help='Nazwa pliku wejściowego.')
 parser.add_argument('output_file', type=str, help='Nazwa pliku wyjściowego.')
 parser.add_argument('format', type=str, help='Format pliku')
-
 args = parser.parse_args()
 
+if args.input_file.endwith('.json'):
+    with open(args.input_file, 'r') as j:
+        try:
+            data = json.load(j)
+        except json.JSONDecodeError as e:
+            print('Niepoprawnny format pliku...')
+            exit(1)
 
